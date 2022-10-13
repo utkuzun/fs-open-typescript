ab1c60c36685dcb461907f9f00bf4a558dc8fde2
utkuzun <utkuzun50@gmail.com>
2022-10-13 13:41:39 +0300

    initial setup for flight diaries

---
+++ flight-diaries/index.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.get('/ping', (_req, res) => {
+  res.send('pong');
+});
+
+const port = process.env.PORT || 3005;
+
+const start = () => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server is listening on ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
