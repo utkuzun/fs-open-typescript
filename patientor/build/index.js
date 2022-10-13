4db714c7313063958494bad0c6ed880180d4fc70
utkuzun <utkuzun50@gmail.com>
2022-10-13 13:47:51 +0300

    setup fix

---
+++ patientor/build/index.js
@@ -0,0 +1,25 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.get('/ping', (_req, res) => {
+    res.send('pong');
+});
+const port = process.env.PORT || 3005;
+const start = () => {
+    try {
+        app.listen(port, () => {
+            console.log(`Server is listening on ${port}`);
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
+};
+start();
